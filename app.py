2cf48799fba23b4b2403bfd5969ef609b6b86725
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2025-02-22 12:27:19 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
@@ -9,6 +11,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///db.
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+api = Api(app)
 
 @app.route("/")
 def home():
