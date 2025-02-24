7b787da54e143f4deb71a581c3f0c698066c0dde
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2025-02-24 13:16:26 +0300

    Rename backref

--- models.py
+++ models.py
@@ -94,7 +94,7 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
-    books = db.relationship('Book', backref='category', lazy=True)
+    books = db.relationship('Book', backref='book_category', lazy=True)
 
     def __repr__(self):
         return f'<Category {self.name}>'
